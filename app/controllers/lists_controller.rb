520e5777f75d89735da59165b8a0a16b9e3ffe47
Ezequiel <epuyosdacosta@gmail.com>
2022-11-13 19:49:58 +0100

    Exercise completed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,6 +22,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to @list, notice: "List was successfully deleted."
+  end
+
   private
 
   def list_params
